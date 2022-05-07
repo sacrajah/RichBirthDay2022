2f9a8722bd8546b1d5e4439c187603ac7451e7a2
Ranger <30779082+sacrajah@users.noreply.github.com>
2022-05-07 22:19:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const countdown = () =>{
+
+    const countDown = new  Date( "May 26 , 2022, 00:00:00").getTime()
+    const now  = new Date().getTime();
+    const gap = countDown- now
+    const second = 1000;
+    const minute = second*60;
+    const hour = minute*60;
+    const day = hour*24;
+    const textDay =Math.floor(gap/day);
+const textHour = Math.floor((gap % day) / hour);
+const textMinute = Math.floor((gap % hour) /minute);
+const textSecond = Math.floor((gap % minute) /second);
+
+    // console.log(countDown);
+    // console.log(textDay);
+    // console.log(textHour);
+    // console.log(textMinute);
+    // console.log(textSecond);
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+
+}
+countdown()
+
+setInterval(countdown, 1000)
